a3a9bc480d52323d495707f7c12a729c091ba825
farju947 <farjanamisty947@gmail.com>
2024-11-07 18:52:28 +0600

    first commit

---
+++ css/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+      darkMode: 'class',
+  content: ["*"],
+  theme: {
+    extend: {
+      "fontFamily":{
+        'Caveat':['"Caveat", cursive;']
+      },
+       "backgroundImage":{
+        'banner':"url('/img/banner.png')"
+       },
+     
+    },
+  },
+  plugins: [],
+}
+
